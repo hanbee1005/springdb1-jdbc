55d3dab6441e4476d663e91b6bee0d6da4ba6c2c
hanbee <sonhanbi1002@gmail.com>
2023-11-02 19:14:05 +0900

    feat: add connection pool test use DataSource

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,7 +1,7 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -38,9 +38,24 @@ public class ConnectionTest {
         useDataSource(dataSource);
     }
 
+    @Test
+    @DisplayName("DataSourceConnectionPool Test")
+    public void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000);
+    }
+
     private void useDataSource(DataSource dataSource) throws SQLException {
-        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
 
         log.info("connection={}, class={}", con1, con1.getClass());
         log.info("connection={}, class={}", con2, con2.getClass());
